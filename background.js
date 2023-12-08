1e5861e41915a43c719771ec980cbb035fd6d773
Paul <dsbrlt2002@gmail.com>
2023-12-08 15:05:08 +0200

    Service worker added

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener((details) =>
+{
+    if(details.reason !== "install" && details.reason !== "update") return;
+
+    console.log("INSTALLED");
+    chrome.webRequest.onCompleted.addListener(async args =>
+        {
+            
+        },
+        {urls: ["https://delta.mil.gov.ua/api/monitor/objects/attachments/*"]}
+    );
+});
